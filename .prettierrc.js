81a1cc23bd6f9d98f1d0b50752b98715bdc68308
Ftleyell <51935035+Ftleyell@users.noreply.github.com>
2025-12-18 21:49:04 +0000

    Deploying to gh-pages from @ Ftleyell/Wisdom-Wiskers@f888e21ee42dba65acbec99d35629974587b6930 🚀

--- .prettierrc.js
+++
@@ -1,9 +0,0 @@
-// .prettierrc.js
-module.exports = {
-    semi: true, // Usar punto y coma
-    trailingComma: 'es5', // Coma final en arrays/objetos
-    singleQuote: true, // Usar comillas simples
-    printWidth: 100, // Ancho de línea
-    tabWidth: 2, // Ancho de tabulación
-    endOfLine: 'auto', // Manejo de fin de línea
-  };
